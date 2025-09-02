cd2641c74654f3363dc4e8d662b20f1c76a04292
Ehsan Hakim <m.ehsan1381@gmail.com>
2025-09-02 14:31:32 +0330

    use image for cup and medal

--- src/components/leader-board/rank-one/index.tsx
+++ src/components/leader-board/rank-one/index.tsx
@@ -1,19 +1,22 @@
-"use client";
+// "use client";
 
-import { Avatar1SVG, LeaderBoardSVG, MedalSVG } from "@/svgs";
+import { Avatar1SVG } from "@/svgs";
+import Image from "next/image";
 
 const RankOne: React.FC = () => {
   return (
     <div className="flex flex-col items-center mt-16 mb-8">
       <div className="flex items-center">
         <span className="text-[1.875rem]">Leader Board</span>
-        <LeaderBoardSVG />
+        <Image alt="medal" src="/assets/cup.png" width={56} height={56} />
+
+        {/* <LeaderBoardSVG /> */}
       </div>
 
       <div className="relative mt-6">
         <Avatar1SVG size="96" />
-        <span className="absolute -bottom-[1.875rem] right-[1.25rem]">
-          <MedalSVG />
+        <span className="absolute -bottom-[1.375rem] right-[1.25rem]">
+          <Image alt="medal" src="/assets/medal.png" width={56} height={56} />
         </span>
       </div>
 
