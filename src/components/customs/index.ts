a535c3e6bfac68a3763f1837dd38d5fc39382246
ehsan <m.ehsan1381@gmail.com>
2025-09-10 13:30:05 +0330

    create custom loader

--- src/components/customs/index.ts
+++ src/components/customs/index.ts
@@ -3,5 +3,6 @@ export { default as CustomIconButton } from "./custom-icon-button";
 export { default as CustomInput } from "./custom-input";
 export { default as CustomPassInput } from "./custom-pass-input";
 export { default as CustomSkeleton } from "./custom-skeleton";
+export { default as CustomLoading } from "./custom-loading";
 
 export * from "./custom-button/type";
